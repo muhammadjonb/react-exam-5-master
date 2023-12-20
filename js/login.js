69515b6494723213d9d67ce5c3795d605c1b9d8c
muhammadjonr <mrahmonaliev22032007@gmail.com>
2023-12-21 04:23:09 +0500

    login page

---
+++ js/login.js
@@ -0,0 +1,14 @@
+const userName = document.querySelector('.username')
+const password = document.querySelector('.password')
+const btnLogin = document.getElementById('btnLogin')
+
+
+
+btnLogin.addEventListener("click",function (e) {
+    e.preventDefault()
+    if(userName.value.length>0 && password.value.length>0){
+        window.location.href="./home.html"
+    }else{
+        alert("Fill in the input data")
+    }
+})
